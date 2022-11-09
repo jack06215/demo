4c02becb2e7032989ff64065f0ef0598a9f1bb4f
Jack Cho <jack06215@gmail.com>
2022-11-09 17:17:30 +0800

    reconstruct error message

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.konan.properties.loadProperties
 
 plugins {
 	id("org.springframework.boot") version "2.7.5"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	id("com.thinkimi.gradle.MybatisGenerator") version "2.4" // 追加
+	id("com.jetbrains.exposed.gradle.plugin") version "0.2.1"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -14,6 +15,7 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
+	jcenter()
 }
 
 dependencies {
@@ -22,12 +24,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	
-	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.4")
-	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1") // 追加
 	implementation("mysql:mysql-connector-java:8.0.23") // 追加
 	implementation("org.springframework.boot:spring-boot-starter-aop")
-	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0") // 追加
-
+	implementation("org.jetbrains.exposed:exposed-core:0.29.1")
+	implementation("org.jetbrains.exposed:exposed-jdbc:0.29.1")
+	implementation("org.jetbrains.exposed:exposed-dao:0.29.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 	testImplementation("org.assertj:assertj-core:3.19.0")
@@ -46,8 +47,21 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+sourceSets.main {
+    java.srcDirs("build/tables")
+}
+
+tasks.generateExposedCode {
+    dependsOn("clean")
+}
+
 // 追加
-mybatisGenerator {
-    verbose = true
-    configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
+exposedCodeGeneratorConfig {
+    val dbProperties = loadProperties("${projectDir}/db.properties")
+    configFilename = "exposedConf.yml"
+		connectionURL = dbProperties["jdbcUrl"].toString()
+    user = dbProperties["dataSource.user"].toString()
+    password = dbProperties["dataSource.password"].toString()
+    databaseName = dbProperties["dataSource.database"].toString()
+    databaseDriver = dbProperties["dataSource.driver"].toString()
 }
